8c1ec88345227da7e65cb655f1085953372c81d1
jaso69 <joseonieva@gmail.com>
2025-09-25 22:40:32 +0200

    cors

--- api/auth/register.js
+++ api/auth/register.js
@@ -1,6 +1,16 @@
 import { AuthService } from '../../lib/auth.js';
 
 export default async function handler(req, res) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Manejar preflight OPTIONS
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Método no permitido' });
     }
