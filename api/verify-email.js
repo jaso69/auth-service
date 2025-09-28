51054cd60f707416dae3ef9d0e0352d04b0a2835
jaso69 <joseonieva@gmail.com>
2025-09-28 15:04:09 +0200

    TEST

--- api/verify-email.js
+++ api/verify-email.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     }
 
     const { token } = req.query;
-
+    // Validar que el token esté presente
     if (!token) {
       return res.status(400).json({ error: 'Token de verificación requerido' });
     }
