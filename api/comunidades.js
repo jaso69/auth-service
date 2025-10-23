2ae5424d3af87f87d753b75c8ab7afd913e28447
jaso69 <joseonieva@gmail.com>
2025-10-23 19:50:47 +0200

    cors comunidades

--- api/comunidades.js
+++ api/comunidades.js
@@ -2,6 +2,14 @@ import { AuthService } from '../../lib/auth.js';
 import { getAllComunidades, getComunidadByNumero } from '../../lib/db.js';
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // Manejar preflight
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Método no permitido' });
   }
